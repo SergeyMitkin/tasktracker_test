b5a68df9350a422e71ea8947056fafa78bc1e27d
SergeyMitkin <sergeymitkin@gmail.com>
2019-12-05 19:47:24 +0300

    Исправлены сортировка и пагинация

---
+++ js/index.js
@@ -0,0 +1,13 @@
+var newURL = location.href.split("?")[1];
+// window.history.pushState('object', document.title, newURL);
+
+// Remove URL Tag Parameter from Address Bar
+if (window.parent.location.href.match(/sort=/)){
+    if (typeof (history.pushState) != "undefined") {
+        var obj = { Title: document.title, Url: window.parent.location.pathname };
+        history.pushState(obj, obj.Title, obj.Url);
+    } else {
+        window.parent.location = window.parent.location.pathname;
+    }
+}
+alert(obj);
