d12c352562445abf75a9fbd8b02a786c018b284e
SergeyMitkin <sergeymitkin@gmail.com>
2019-12-12 19:54:34 +0300

    Исправлена функция редактирования задачи

--- js/index.js
+++ js/index.js
@@ -1,13 +1,10 @@
-//var newURL = location.href.split("?")[1];
-// window.history.pushState('object', document.title, newURL);
 
 // Remove URL Tag Parameter from Address Bar
-if (window.parent.location.href.match(/res=/)){
-    if (typeof (history.pushState) != "undefined") {
-        var obj = { Title: document.title, Url: window.parent.location.pathname };
+if (window.parent.location.href.match(/res=updated/)){
+    if (typeof (history.pushState) != "undefined"){
+        var obj = { Title: document.title, Url: window.parent.location.pathname};
         history.pushState(obj, obj.Title, obj.Url);
     } else {
         window.parent.location = window.parent.location.pathname;
     }
 }
-//alert(obj);
