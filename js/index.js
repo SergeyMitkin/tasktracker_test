16c40b9056df866df1636eb856f48695c4c68780
SergeyMitkin <sergeymitkin@gmail.com>
2019-12-15 21:51:50 +0300

    Добавлены даты создания и выполнения в карточки задач

--- js/index.js
+++ js/index.js
@@ -1,6 +1,6 @@
 
 // Remove URL Tag Parameter from Address Bar
-if (window.parent.location.href.match(/res=updated/)){
+if (window.parent.location.href.match(/res/)){
     if (typeof (history.pushState) != "undefined"){
         var obj = { Title: document.title, Url: window.parent.location.pathname};
         history.pushState(obj, obj.Title, obj.Url);
--- views/v_create.php
+++ views/v_create.php
@@ -7,6 +7,7 @@
 ?>
 <?=$response?>
 <h3>Создайте задачу</h3>
+
 <form method="post" action ='?act=create'/> Выберете ответсвенного
     <select name="name">
     <?php
@@ -15,7 +16,20 @@
     }
     ?>
     </select>
-<p><input type="text" name="task_name" value="Название задачи"></p>
-<p><input type="text" name="description" value="Текст задачи"></p>
-<p><input type="submit" value="Отправить"/></p>
+
+    <p><input type="text" name="task_name" value="<?=$task_name?>"></p>
+    <p><input type="text" name="description" value="<?=$task_description?>"></p>
+
+    <p>
+        <label for="date">Срок выполнения: </label>
+        <input type="date" id="date" name="date" value="<?=$date?>"/>
+    </p>
+
+    <p>
+        <label for="time">Время: </label>
+        <input type="time" id="time" name="time" value="<?=$time?>"/>
+    </p>
+
+    <p><input type="submit" value="Отправить"/></p>
+
 </form>
