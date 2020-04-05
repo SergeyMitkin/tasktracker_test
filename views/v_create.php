d550175c6211490cb9eab646a648726ac6193bfc
SergeyMitkin <sergeymitkin@gmail.com>
2020-04-05 19:13:22 +0300

    Созданы стили для карточки задачи, формы создания задачи и редактирования

--- views/v_create.php
+++ views/v_create.php
@@ -5,33 +5,71 @@
  * $text - текст
  */
 ?>
-<?=$response?>
 
-<form method="post" action ='?act=create'/> Выберете ответсвенного
-    <select name="name">
-    <?php
-    foreach($usersData as $name) {
-    echo '<option value="' . $name['user_id'] . '">' . $name['user_name'] . '</option>';
-    }
-    ?>
-    </select>
+<div class="create-container">
+    <?=$response?>
 
-    <br><br>
-    <label for="task_name">Введите название задачи </label>
-    <input type="text" id="task_name" name="task_name" value="<?=$task_name?>" placeholder="Название задачи">
+    <form method="post" action ='?act=create'/>
+        <h1><?=$create_or_update?></h1>
 
-    <br><br>
-    <label for="description">Введите описание задачи </label>
-    <input type="text" name="description" value="<?=$task_description?>" placeholder="Описание задачи">
+            <?
+            if($_GET['edit'] == 'on'){
+                ?>
 
-    <br><br>
-    <label for="date">Срок выполнения: </label>
-    <input type="date" id="date" name="date" value="<?=$date?>"/>
+                <div class="group">
+                    <p>Ответственный: <?=$user_name?></p>
+                </div>
 
-    <br><br>
-    <label for="time">Время: </label>
-    <input type="time" id="time" name="time" value="<?=$time?>"/>
+                <div class="group">
+                    <label for="task_name">Название задачи </label>
+                    <input type="text" id="task_name" name="task_name" value="<?=$task_name?>" placeholder="Название задачи">
+                </div>
 
-    <br><br>
-    <input class="btn btn-success" type="submit" value="Отправить"/>
-</form>
+                <div class="group">
+                    <label for="description">Описание задачи </label>
+                    <textarea name="description" value="<?=$task_description?>" placeholder="Описание задачи"><?=$task_description?></textarea>
+                </div>
+
+                <div class="group">
+                    <p>Срок выполнения: <?=$dead_line?></p>
+                    <input type="hidden" id="dead_line" name="dead_line" value="<?=$dead_line?>">
+                </div>
+
+            <?}else{?>
+
+                <div class="group">
+                    <label for="name">Выберете ответсвенного</label>
+                    <select id="name" name="name">
+                    <?php
+                    foreach($usersData as $name) {
+                    echo '<option value="' . $name['user_id'] . '">' . $name['user_name'] . '</option>';
+                    }
+                    ?>
+                    </select>
+                </div>
+
+                <div class="group">
+                    <label for="task_name">Введите название задачи </label>
+                    <input type="text" id="task_name" name="task_name" value="<?=$task_name?>" placeholder="Название задачи">
+                </div>
+
+                <div class="group">
+                    <label for="description">Введите описание задачи </label>
+                    <textarea name="description" value="<?=$task_description?>" placeholder="Описание задачи"></textarea>
+                </div>
+
+                <div class="group">
+                    <label for="date">Срок выполнения: </label>
+                    <input type="date" id="date" name="date" value="<?=$date?>"/>
+                </div>
+
+                <div class="group">
+                    <label for="time">Время: </label>
+                    <input type="time" id="time" name="time" value="<?=$time?>"/>
+                </div>
+            <?}?>
+        <div class="group" align="center">
+            <button class="btn btn-light">Отправить</button>
+        </div>
+    </form>
+</div>
