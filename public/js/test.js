9acf92fa330e324beafbd1cbedbc09ecd4b11d0c
SergeyMitkin <sergeymitkin@gmail.com>
2020-07-21 21:58:37 +0300

    Удалены лишние файлы

--- public/js/test.js
+++
@@ -1,6 +0,0 @@
-/*
-var prices = [1, 2, 125, 19, 14, 156];
-console.log(prices.sort(function (a, b) {
-    return a-b;
-}));
-*/
--- public/js/testjs.php
+++
@@ -1,6 +0,0 @@
-<?php
-$arr['a'] = 10;
-$arr['b'] = 20;
-$arr['c'] = 30;
-
-echo json_encode($arr);
